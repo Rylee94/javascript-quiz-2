28937f4a7badf47c5cb1440895f98c1acadeda34
Rylee94 <128653866+Rylee94@users.noreply.github.com>
2023-05-20 23:34:47 -0500

    starter code for quiz

---
+++ assets/js/quiz-questions.js
@@ -0,0 +1,24 @@
+var questions = [
+  {
+    question: "Which is NOT a JS data type.",
+    choices: ["pickles", "booleans", "strings", "integers"],
+    answer: "pickles",
+  },
+  {
+    question: "Which is NOT a JS data type.",
+    choices: ["pickles", "booleans", "strings", "integers"],
+    answer: "pickles",
+  },
+  {
+    question: "Which is NOT a JS data type.",
+    choices: ["pickles", "booleans", "strings", "integers"],
+    answer: "pickles",
+  },
+];
+
+questions[currentQuestion].question;
+questions[currentQuestion].choices;
+questions[currentQuestion].answer;
+
+
+//figure out logic for quiz and displaying questions and the logic for timer
